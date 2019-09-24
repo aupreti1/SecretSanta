76d349fe3a8bf0ecdc337568ebdd83b4cb631aa9
aupreti1 <anaupreti@gmail.com>
2019-09-23 23:52:38 -0400

    Got the function of the app working

--- assets/javascript/santa.js
+++ assets/javascript/santa.js
@@ -3,8 +3,27 @@ var names = ["Ana", "Alex", "Brittany",
 "Seth", "Sean", "Shelby", "Zachary",
 "McKenzie", "Shyanne", "Luke"]
 
-function randomName(names) {
-    return names[Math.floor(Math.random() * names.length)];
+var split = function(i, xs) {
+    var a = xs.slice(0, i);
+    var b = xs.slice(i, xs.length);
+    return [a, b];
+};
+
+var shuffle = function(xs) {
+    return xs.splice(0).sort(function() {
+        return .5 - Math.random();
+    });
+};
+
+var zip = function(xs) {
+    return xs[0].map(function(_, i) {
+        return xs.map(function(x) {
+            return x[i];
+        });
+    });
 }
 
-console.log(randomName(names));
+var result = zip(split(names.length/2, shuffle(names)));
+
+console.log(result);
+
