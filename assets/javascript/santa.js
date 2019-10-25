ad0963da7361b68e033b0709f2d2f97ff8a54f9e
aupreti1 <anaupreti@gmail.com>
2019-10-24 23:41:38 -0400

    App generates name on click

--- assets/javascript/santa.js
+++ assets/javascript/santa.js
@@ -3,37 +3,13 @@ var names = ["Ana", "Alex", "Brittany",
 "Seth", "Sean", "Shelby", "Zachary",
 "McKenzie", "Shyanne", "Luke"]
 
-if (names.length % 2 != 0) {
-    connsole.log("You must have an even number of names. You currently have " + names.length + " names.");
-} else {
-    var arr1 = names.slice(), // copy array
-        arr2 = names.slice(); // copy array again
-
-    arr1.sort(function() { return 0.5 - Math.random();}); // shuffle arrays
-    arr2.sort(function() { return 0.5 - Math.random();});
-
-    while (arr1.length) {
-        var name1 = arr1.pop(), 
-            name2 = arr2[0] == name1 ? arr2.pop() : arr2.shift();
-            
-        console.log(name1 + ' gets ' + name2);
+function spin() {
+    if(names.length == 0) {
+        alert("All Names Have Been Matched!!");
+        generateNumbers();
     }
+    var random = Math.floor(Math.random() * names.length);
+    var num = names[random];
+    names.splice(random, 1);
+    document.getElementById("names").innerHTML = num;
 }
-
-document.write(name1 + ' gets ' + name2);
-
-//Done for the night
-function nextPair(current, names) {
-    var index = names.indexOf(current)
-    if (index === names.length - 1) {
-        return names[0]
-    }
-    return names[index = 1]
-}
-
-next.addEventListener('click', function() {
-    var newSanta = nextPair(name1 + name2, names)
-    (name1 + ' gets ' + name2) = newSanta
-})
-
-
