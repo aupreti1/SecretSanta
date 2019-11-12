ce4ead00457b9706835cc4e968fb8ec58b4fa9a8
aupreti1 <anaupreti@gmail.com>
2019-11-12 14:53:33 -0500

    Added CSS

--- assets/javascript/santa.js
+++ assets/javascript/santa.js
@@ -1,43 +1,142 @@
-var names = ["Ana", "Alex", "Brittany", 
-"Reece", "Justin", "Deana", "BileeJane",
-"Seth", "Sean", "Shelby", "Zachary",
-"McKenzie", "Shyanne", "Luke"]
-
-var currentItemIndex = 14;
-var myItemObjectArray = match;
-
-
-if (names.length % 2 != 0) {
-    console.log("You must have an even number of names. You currently have " + names.length + " names.");
-} else {
-    var arr1 = names.slice(), 
-        arr2 = names.slice(); 
-
-    arr1.sort(function() { return 0.5 - Math.random();}); 
-    arr2.sort(function() { return 0.5 - Math.random();});
-
-    while (arr1.length) {
-        var name1 = arr1.pop(), 
-            name2 = arr2[0] == name1 ? arr2.pop() : arr2.shift();
-
-        var match = name1 + 'gets' + name2;
-
-        console.log(name1 + ' gets ' + name2);
+function assignSantas(array) {
+    var matches = [];
+  
+    if(!array || !array.length) {
+      return null;
     }
-}
+  
+    var santas = array.slice();
+    shuffle(santas);
+  
+    for(var i=0; i<santas.length; i++) {
+      var santa = santas[i],
+          recipient;
+  
+      // Assign santa to the person next to them to avoid assigning to self and avoid duplicate recipients
+      if(i !== santas.length-1) {
+        recipient = santas[i+1];
+      } else {
+        recipient = santas[0];      
+      }
+  
+      matches.push({ "santa": santa, "recipient": recipient });
+    }
+  
+    return matches;
+  };
+  
+  function shuffle(array) {
+    var n = array.length, 
+        i, 
+        j;
+  
+    while(n) {
+      i = Math.floor(Math.random() * n--);
+  
+      j = array[n];
+      array[n] = array[i];
+      array[i] = j;
+    } 
+  }
+  
+  function displayNames(array) {
+    var list = document.getElementById('santa-list');
+    var input = document.getElementById('santa-input');
+    input.value = '';
+    list.innerHTML = '';
+  
+    for(var i=0; i<array.length; i++) {
+      var pairing   = array[i],
+          santa     = pairing.santa,
+          recipient = pairing.recipient,
+          dt        = document.createElement('dt'),
+          dd        = document.createElement('dd');
+  
+        dt.innerHTML += pairing.santa;
+        dd.innerHTML += 'buys a gift for <strong>' + pairing.recipient + '</strong>';
+        list.appendChild(dt);
+        list.appendChild(dd);
+    }
+  }
+  
+  function parseNames(value) {
+    var names = [];
+    var vals = value.split('\n');
+  
+    for (var i=0; i<vals.length; i++) {
+      var name = vals[i].trim();
+      if (name === '') continue;
+      names.push(name);
+    }
+    return names;
+  }
+  
+  if (typeof(document) !== 'undefined') {
+    var button  = document.getElementById('santa-submit');
+  
+    button.addEventListener('click', function(e){
+      e.preventDefault();
+      var input = document.getElementById('santa-input');
+      var errorDiv = document.getElementById('error');
+      var names = parseNames(input.value);
+  
+      if(names.length < 2) {
+        input.classList.add('is-error');
+        errorDiv.innerHTML = 'Please enter at least 2 names';
+        errorDiv.classList.add('is-active');
+      } else {
+        input.classList.remove('is-error');
+        errorDiv.classList.remove('is-active');
+        var matches = assignSantas(names);
+        displayNames(matches);
+      }
+    });
+  }
+  
+  if (typeof(module) !== 'undefined') {
+    module.exports.assignSantas = assignSantas;
+    module.exports.parseNames = parseNames;
+  }
 
-document.write(name1);
+// var currentItemIndex = 14;
+// var myItemObjectArray = match;
 
-function showPartner() {
-  document.getElementById("name2").innerHTML = name2;
-}
+// if (names.length % 2 != 0) {
+//     console.log("You must have an even number of names. You currently have " + names.length + " names.");
+// } else {
+//     var arr1 = names.slice(), 
+//         arr2 = names.slice(); 
 
-index = match.indexOf();
-if(index >= 0 && index < match.length - 1)
-   nextItem = match[index + 1]
-   document.getElementById("match").innerHTML = match;
-   
+//     arr1.sort(function() { return 0.5 - Math.random();}); 
+//     arr2.sort(function() { return 0.5 - Math.random();});
 
+//     while (arr1.length) {
+//         var name1 = arr1.pop(), 
+//             name2 = arr2[0] == name1 ? arr2.pop() : arr2.shift();
 
+//         var match = name1 + 'gets' + name2;
+
+//         console.log(name1 + ' gets ' + name2);
+//     }
+// }
+
+// document.write(name1);
+
+// function showPartner() {
+//   document.getElementById("name2").innerHTML = 'gets ' + name2;
+// }
+
+// var Current = 0;
+
+//     document.getElementById("match");
+
+//     function Next(){
+//         if(Current == match.length - 1){
+//             Current = 0}
+//         else{
+//             Current++;}
+
+//         document.getElementById("match");
+//     }
 
 
